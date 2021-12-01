e4796d1f818304e270ddf285ff8d2f4373af15ad
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-01 23:25:35 +0200

    added rma base add form

---
+++ resources/views/pages/add-rma-base.blade.php
@@ -0,0 +1,42 @@
+@extends('main')
+@section('content')
+    <div class="container">
+        <form action="store-rma-base" method="POST" class="mt-5">
+            @csrf
+            @include("_partials/errors")
+            <h5 class="text-center" style="font-size:200%">Fill out the form!</h5>
+
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" name="name" id="name">
+            </div>
+
+            <label for="phone" class="form-label">Phone</label>
+            <div class="input-group mb-3">
+                <span class="input-group-text" style="border: 1px solid #6b7280;"><i class="fas fa-mobile-alt"></i></span>
+                <input type="text" class="form-control" placeholder="(+370)000-000-00" name="phone" id="phone">
+            </div>
+
+            <div class="mb-3">
+                <label for="deviceProductNumber" class="form-label">Device P/N</label>
+                <input type="text" class="form-control" name="deviceProductNumber" id="deviceProductNumber">
+            </div>
+
+            <div class="mb-3">
+                <label for="deviceSerialNumber" class="form-label">Device S/N</label>
+                <input type="text" class="form-control" name="deviceSerialNumber" id="deviceSerialNumber">
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="4"></textarea>
+            </div>
+
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary mb-3">Submit</button>
+            </div>
+            
+        </form>
+    </div>
+    <div class="mb-5"></div>
+@endsection
