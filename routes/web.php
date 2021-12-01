5f89eabd8bccbbe125165f65e61a9f07a2c86edc
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-01 23:24:26 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\DLinkController;
+use App\Http\Controllers\RMAController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,10 +22,22 @@ Route::get('/', [DLinkController::class, 'index']);
 Route::get('/clients', [ClientController::class, 'allClients']);
 Route::get('/add-client', [ClientController::class, 'newClientForm']);
 Route::post('/store-client', [ClientController::class, 'storeClient']);
+Route::post('/search-client', [ClientController::class, 'askSearchClient']);
+Route::get('/search-client/{searchString}', [ClientController::class, 'searchClient']);
 Route::get('/client/{client}/delete/ask', [ClientController::class, 'viewRemoveClientForm']);
 Route::get('/client/{client}/delete/confirm', [ClientController::class, 'deleteClientForm']);
 Route::get('/client/{client}/edit', [ClientController::class, 'viewEditClientForm']);
 Route::patch('/client/{client}/edit', [ClientController::class, 'updateClientForm']);
+
+// RMA CONTROLLER
+Route::get('/rma', [RMAController::class, 'viewRmaBases']);
+Route::get('/add-rma-base', [RMAController::class, 'newRmaBaseForm']);
+Route::post('/store-rma-base', [RMAController::class, 'storeRmaBase']);
+Route::get('/rma/{rma}', [RMAController::class, 'viewRmaBase']);
+Route::get('/rma/{rma}/delete/ask', [RMAController::class, 'viewRemoveRmaBaseForm']);
+Route::get('/rma/{rma}/delete/confirm', [RMAController::class, 'deleteRmaBaseForm']);
+Route::get('/rma/{rma}/edit', [RMAController::class, 'viewEditRmaBaseForm']);
+Route::patch('/rma/{rma}/edit', [RMAController::class, 'updateRmaBaseForm']);
 
 // Dashboard
 Route::get('/dashboard', [DLinkController::class, 'dashboard'])->name('dashboard');
