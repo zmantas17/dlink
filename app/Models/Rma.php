baf6fc66123a4e16c4be2035b4f25c46fa931c36
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-01 23:23:29 +0200

    Rma & Client models

--- app/Models/Rma.php
+++ app/Models/Rma.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Client extends Model
+class Rma extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'address', 'phone', 'email', 'contactName', 'contactPhone', 'notes', 'owner'];
+    protected $fillable = ['name', 'phone', 'deviceProductNumber', 'deviceSerialNumber', 'description'];
 }
