64493942a810268346211dbcbbefcd2cd8b25ba0
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-01 23:23:03 +0200

    client controller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
+use DB;
 
 class ClientController extends Controller
 { 
@@ -24,50 +26,47 @@ class ClientController extends Controller
         return view('pages.add-client');
     }
 
-    public function storeClient(Request $request)
+    public function storeClient(Request $request, Client $client)
     {
-        $validated = $request->validate([
-            'name' => 'required|max:255',
-            'address' => 'required',
-            'phone' => 'required',
-            'email' => 'required|email',
-            'contactName' => 'required',
-            'contactPhone' => 'required',
-            'notes' => 'required'
-        ]);
+            $validated = $request->validate([
+                'name' => 'required|max:255',
+                'address' => 'required',
+                'phone' => 'required',
+                'email' => 'required|email',
+                'contactName' => 'required',
+                'contactPhone' => 'required',
+                'notes' => 'required'
+            ]);
 
-        Client::create([
-            'name' => request('name'),
-            'address' => request('address'),
-            'phone' => request('phone'),
-            'email' => request('email'),
-            'contactName' => request('contactName'),
-            'contactPhone' => request('contactPhone'),
-            'notes' => request('notes')
-        ]);
-        return redirect('/');
-    }
+            Client::create([
+                'name' => request('name'),
+                'address' => request('address'),
+                'phone' => request('phone'),
+                'email' => request('email'),
+                'contactName' => request('contactName'),
+                'contactPhone' => request('contactPhone'),
+                'notes' => request('notes'),
+                'owner' => Auth::id()
+            ]);
+            return redirect('/');
+}
     
-    //! Are you sure delete?
     public function viewRemoveClientForm(Client $client) 
     {
-        return view('pages.view-remove', compact('client'));
+            return view('pages.view-remove', compact('client'));
     }
 
-    //! Remove
     public function deleteClientForm(Client $client)
     {
         $client->delete();
         return redirect('/');
     }
 
-    //! Edit
     public function viewEditClientForm(Client $client)
     {
         return view("pages.client-form-edit", compact('client'));
     }
 
-    //! Update
     public function updateClientForm(Request $request, Client $client)
     {
             $validated = $request->validate([
@@ -81,7 +80,21 @@ class ClientController extends Controller
             ]);
 
             Client::where('id', $client->id)->update($request->only(['name', 'address', 'phone', 'email', 'contactName', 'contactPhone', 'notes']));
+            Client::where('id', $client->id)->update(['owner' => Auth::id()]);
 
             return redirect('/clients');
+    }
+
+    public function askSearchClient(Request $request)
+    {
+        return redirect('/search-client/' . $request->get('search-string'));
+    }
+
+    public function searchClient($searchString)
+    {
+        $clients = Client::where(DB::raw('lower(name)'), strtolower($searchString))->get();
+
+
+        return view('pages.view-clients', compact('clients'));
     }
 }
