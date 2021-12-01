660d30ffed17e5b7109c0025931dc13bb14ac33f
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-01 23:26:12 +0200

    view clients search

--- resources/views/pages/view-clients.blade.php
+++ resources/views/pages/view-clients.blade.php
@@ -1,10 +1,29 @@
 @extends('main')
 @section('content')
 <div class="container mt-5">
-    <div class="text-right mb-3">
-        <a href="/add-client" class="btn btn-success ">Add <i class="fas fa-plus-circle"></i></a>
+    <div class="row">
+        <div class="col">
+            <a href="/add-client" class="btn btn-success ">Add <i class="fas fa-plus-circle"></i></a>
+        </div>
+        <div class="col">
+            <div class="text-right mb-3">
+                <a class="btn btn-dark hover:bg-transparent border-0" id="search-button"><i class="fas fa-search"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <form action="/search-client" method="POST" id="search-form" class="fadeOut" style="animation-duration: 1s">
+            @csrf
+            <input class="text-dark mb-2 rounded w-full" type="text" id="search-string" name="search-string" placeholder="Search">
+        </form>
     </div>
     <div class="accordion text-dark" id="accordionExample">
+        @if($clients->count() == 0)
+            <div class="alert alert-secondary" role="alert">
+                No client found!
+            </div>
+        @endif
+
         @foreach($clients as $client)        
             <div class="accordion-item mb-2">
                 <h2 class="accordion-header" id="headingTwo">
@@ -20,10 +39,18 @@
                         <p><b>Email:</b> {{ $client->email }}</p>
                         <p><b>Contact Name:</b> {{ $client->contactName }}</p>
                         <p><b>Contact Phone:</b> {{ $client->contactPhone }}</p>
-                        <p class="mb-3"><b>Notes:</b> {{ $client->notes }} </p>
+                        <p class="mb-2"><b>Notes:</b> {{ $client->notes }} </p>
                         <div class="text-center">
-                                <a href="/client/{{ $client->id }}/delete/ask" class="btn btn-sm btn-danger "><i class="fas fa-trash"></i></a>
-                                <a href="/client/{{ $client->id }}/edit" class="btn btn-sm btn-warning "><i class="fas fa-pen"></i></a>
+                            <div class="row">
+                                <div class="col-sm-4"></div>
+                                <div class="col-sm-4">
+                                    <a href="/client/{{ $client->id }}/delete/ask" class="btn btn-sm btn-danger "><i class="fas fa-trash"></i></a>
+                                    <a href="/client/{{ $client->id }}/edit" class="btn btn-sm btn-warning "><i class="fas fa-pen"></i></a>
+                                </div>
+                                <div class="col-sm-4">
+                                    <p class="mb-3"><b>Last updated by:</b> {{App\Models\User::where('id', $client->owner)->get()[0]->name}}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -32,4 +59,16 @@
         <div class="mb-5"></div>
     </div>
 </div>
+
+<script>
+
+const searchButton = document.getElementById('search-button');
+const searchForm = document.getElementById('search-form');
+
+searchButton.addEventListener('click', () => {
+    searchForm.classList.toggle('fadeOut');
+    searchForm.classList.toggle('fadeIn');
+});
+
+</script>
 @endsection
