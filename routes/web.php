f3a25e336e907b6e60d1df740be8b51ac43fd369
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-08 20:47:19 +0200

    added routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DemoBaseController;
 use App\Http\Controllers\DLinkController;
 use App\Http\Controllers\RMAController;
+use App\Http\Controllers\SerialNumberController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// DLinkController
+// DLink Controller
 Route::get('/', [DLinkController::class, 'index']);
 
-// ClientController
+// Client Controller
 Route::get('/clients', [ClientController::class, 'allClients']);
 Route::get('/add-client', [ClientController::class, 'newClientForm']);
 Route::post('/store-client', [ClientController::class, 'storeClient']);
@@ -29,7 +31,7 @@ Route::get('/client/{client}/delete/confirm', [ClientController::class, 'deleteC
 Route::get('/client/{client}/edit', [ClientController::class, 'viewEditClientForm']);
 Route::patch('/client/{client}/edit', [ClientController::class, 'updateClientForm']);
 
-// RMA CONTROLLER
+// RMA Controller
 Route::get('/rma', [RMAController::class, 'viewRmaBases']);
 Route::get('/add-rma-base', [RMAController::class, 'newRmaBaseForm']);
 Route::post('/store-rma-base', [RMAController::class, 'storeRmaBase']);
@@ -38,6 +40,23 @@ Route::get('/rma/{rma}/delete/ask', [RMAController::class, 'viewRemoveRmaBaseFor
 Route::get('/rma/{rma}/delete/confirm', [RMAController::class, 'deleteRmaBaseForm']);
 Route::get('/rma/{rma}/edit', [RMAController::class, 'viewEditRmaBaseForm']);
 Route::patch('/rma/{rma}/edit', [RMAController::class, 'updateRmaBaseForm']);
+
+// Demo Base Controller
+Route::get('/demo-base', [DemoBaseController::class, 'demoBase']);
+Route::get('/add-demo-device', [DemoBaseController::class, 'newDemoDevice']);
+Route::post('/store-demo-device', [DemoBaseController::class, 'storeDemoDevice']);
+Route::get('/add-demo-contract', [DemoBaseController::class, 'newDemoContract']);
+Route::post('/store-demo-contract', [DemoBaseController::class, 'storeDemoContract']);
+Route::get('/view-demo-contract', [DemoBaseController::class, 'viewDemoContract']);
+Route::get('/contract/{contract}/finish', [DemoBaseController::class, 'finishDemoContract']);
+Route::get('/contract/{contract}/resume', [DemoBaseController::class, 'resumeDemoContract']);
+Route::get('/contract/{contract}/delete/confirm', [DemoBaseController::class, 'deleteDemoContract']);
+Route::get('/view-demo-contract/finished', [DemoBaseController::class, 'finishedDemoContract']);
+Route::get('/view-demo-contract/notfinished', [DemoBaseController::class, 'notFinishedDemoContract']);
+
+// Serial Number Controller
+Route::get('/serial-number', [SerialNumberController::class, 'viewSerialNumber']);
+Route::post('/store-serial-number', [SerialNumberController::class, 'storeSNFile']);
 
 // Dashboard
 Route::get('/dashboard', [DLinkController::class, 'dashboard'])->name('dashboard');
