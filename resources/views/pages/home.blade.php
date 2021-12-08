74447a2ae8001f2bb1e4423c977c286b4d85934b
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-08 20:43:48 +0200

    changed design colors

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,31 +3,31 @@
 <header class="header">
     <div class="container text-center text-white spaces ">
         <div class="mt-3 space">  
-            <a href="/clients" class="btn btn-dlink text-dark rounded-pill">
-               <i>Clients</i>   <i class="far fa-address-book"></i>
+            <a href="/clients" class="btn btn-dlink rounded-pill">
+               <i>Clients</i> <i class="far fa-address-book"></i>
             </a>    
         </div>
         <div class="mt-3 space">  
-            <a href="/rma" class="btn btn-dlink text-dark rounded-pill">
+            <a href="/rma" class="btn btn-dlink rounded-pill">
                <i>RMA Base</i>   <i class="fas fa-database"></i>
             </a>    
         </div>
         <div class="mt-3 space">  
-            <a href="/" class="btn btn-dlink text-dark rounded-pill">
+            <a href="/demo-base" class="btn btn-dlink rounded-pill">
                 <i>Demo Base</i>  <i class="far fa-file-alt"></i>
             </a>    
         </div>
         <div class="mt-3 space">  
-            <a href="/" class="btn btn-dlink text-dark rounded-pill">
+            <a href="/serial-number" class="btn btn-dlink rounded-pill">
                 <i>S/N Import</i>  <i class="fas fa-align-center"></i>
             </a>    
         </div>
     </div>
-    <div class="space-top text-center text-white pb-5">  
-        <a href="/dashboard" class="btn btn-dlink mr-5 text-dark rounded-pill">
+    <div class="space-top text-center text-white">  
+        <a href="/dashboard" class="btn btn-dlink mr-5 rounded-pill">
             <i><b>Admin</b></i>  <i class="fas fa-user-cog"></i>
         </a>  
-        <a href="/logout" class="btn btn-dlink ml-5 text-dark rounded-pill">
+        <a href="/logout" class="btn btn-dlink ml-5 rounded-pill">
             <i><b>Logout</b></i>  <i class="fas fa-sign-out-alt"></i>
         </a>    
     </div> 
