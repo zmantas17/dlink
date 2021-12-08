585a8435fc4b6c8257b6504b5bf7e4e2046442f4
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-08 20:43:30 +0200

    added demo base page

---
+++ resources/views/pages/demo-base.blade.php
@@ -0,0 +1,22 @@
+@extends('main')
+@section('content')
+<header class="header">
+    <div class="container text-center spaced">
+        <div class="mt-3 ">  
+            <a href="/add-demo-device" class="btn btn-dlink rounded-pill text-2xl">
+               <i>New demo device</i> <i class="fas fa-plus-circle"></i>
+            </a>    
+        </div>
+        <div class="mt-3">  
+            <a href="/add-demo-contract" class="btn btn-dlink rounded-pill text-2xl">
+               <i>New demo contract</i> <i class="fas fa-file-contract"></i>
+            </a>    
+        </div>
+        <div class="mt-3">  
+            <a href="/view-demo-contract" class="btn btn-dlink rounded-pill text-2xl">
+                <i>View demo contracts</i>  <i class="fas fa-eye"></i>
+            </a>    
+        </div>
+    </div>
+</header>
+@endsection
