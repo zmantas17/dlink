d8a97cadb01b6e3e10ce8c51edc597712eae5a3b
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-08 20:46:29 +0200

    changed colors & design

--- resources/views/pages/view-rma-base.blade.php
+++ resources/views/pages/view-rma-base.blade.php
@@ -2,15 +2,15 @@
 @section('content')
 
 <div class="container d-flex justify-content-center mt-5">
-    <div class="card bg-dark border-radius text-white border pb-2" style="width: 18rem;">
-        <div class="card-body">
-            <p><b>Name:</b> {{ $rma->name }}</p>
-            <p><b>Phone:</b> {{ $rma->phone }}</p>
-            <p><b>Device P/N:</b> {{ $rma->deviceProductNumber }}</p>
-            <p><b>Device S/N:</b> {{ $rma->deviceSerialNumber }}</p>
+    <div class="card border-radius bg-transparent" style="width: 18rem; border: 1px solid #58dcfa;">
+        <div class="card-body text-center">
+            <p class="my-2" style="font-size: 1.5rem;">{{ $rma->name }}</p>
+            <p><b style="color: #58dcfa;">Phone:</b> {{ $rma->phone }}</p>
+            <p><b style="color: #58dcfa;">Device P/N:</b> {{ $rma->deviceProductNumber }}</p>
+            <p><b style="color: #58dcfa;">Device S/N:</b> {{ $rma->deviceSerialNumber }}</p>
         </div>
-        <div class="text-center">
-            <a href="{{ url()->previous() }}" class="btn btn-sm btn-success "><i class="fas fa-arrow-circle-left"></i></a>
+        <div class="text-center mb-3">
+            <a href="{{ url()->previous() }}" class="btn" style="color: #58dcfa; border: 1px solid #58dcfa;"><i class="fas fa-arrow-circle-left"></i></a>
         </div>
     </div>
 </div>
