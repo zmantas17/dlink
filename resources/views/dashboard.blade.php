fa971f9b38306a72d0c2ab001224decca83bd4a4
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-09 23:13:12 +0200

    changed dashboard design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,11 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+    <div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+            <div class="overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="mt-5">
+                    <div class="alert text-white text-center p-5" role="alert">
+                        <h4 class="alert-heading ml-3 p-5" style="font-size: 150%">Welcome to Admin Panel!</h4>
+                    </div>
                 </div>
             </div>
         </div>
